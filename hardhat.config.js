a05dd7cdaf193e1e86e7def69b09a5e8c6326f33
Cem <cem.denizsel@gmail.com>
2023-03-13 00:01:01 +0100

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.18",
+// };
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.7.3",
+   defaultNetwork: "sepolia",
+   networks: {
+      hardhat: {},
+      sepolia: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
